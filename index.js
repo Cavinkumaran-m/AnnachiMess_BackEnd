5fe79d0f344741b7a09d33c718788b7465b20518
Cavinkumaran M <82271718+Cavinkumaran-m@users.noreply.github.com>
2023-05-27 13:08:28 +0530

    init

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const mongoDBSession = require("connect-mongodb-session")(session);
+const Route = require("./Routes/CustomerRoutes");
+const cors = require("cors");
+const app = express();
+const crypt = require("bcrypt");
+app.use(express.json());
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000"],
+//     credentials: true,
+//     methods: ["GET", "POST"],
+//   })
+// );
+
+app.use(cors());
+// Database Connection
+const uri =
+  "mongodb+srv://cavinkumaran1257:CAVIN1981@maincluster.ybeixlr.mongodb.net/?retryWrites=true&w=majority";
+
+// const uri = "mongodb://localhost:27017/Annachi";
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Session Handling
+const Store = new mongoDBSession({
+  uri: uri,
+  collection: "AnnachiSessions",
+});
+
+app.use(
+  session({
+    secret: "secretKeyForCavinSession12345",
+    resave: false,
+    saveUninitialized: false,
+    store: Store,
+    cookie: {
+      expires: 1000 * 60 * 5,
+    },
+  })
+);
+
+app.use("/", Route);
+
+app.get("status", (req, res) => {
+  res.send("API version 1.01");
+});
+
+app.listen(process.env.PORT || 80, () => {
+  console.log("Server live and running @ " + process.env.PORT + " & " + 80);
+});
